84f48aea148dc8d46d18bc4d2093109203716b6b
Getulio <getuliovdmnaweb@gmail.com>
2020-10-18 23:17:14 -0300

    Adding logo action to go back to home url

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {useStyles} from './styles'
+import { useStyles } from "./styles";
 import logo from "../../assets/logo.png";
-
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
   const styles = useStyles();
 
   return (
     <div className={styles.appBar}>
-      <img src={logo} alt="logo" />
+      <Link style={{ textDecoration: "none", display: 'flex' }} to="/">
+        <img src={logo} alt="logo" />
+      </Link>
       GNARBOX
     </div>
   );
