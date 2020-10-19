f1bb657f9fa73b1c31bcaab3ee857392638ded0b
Getulio <getuliovdmnaweb@gmail.com>
2020-10-18 21:55:42 -0300

    Creating detailCard component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { createUseStyles } from "react-jss";
 import { AppHeader, AppBody } from "./components";
-import { Switch, Route, Link, Redirect } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams,
+  Redirect,
+} from "react-router-dom";
+import Axios from "axios";
 
 const useStyles = createUseStyles({
   appContainer: {
-    position: "absolute",
+    backgroundColor: "#E5E5E5",
+    height: "100vh",
+  },
+  detailBody: {
     backgroundColor: "#E5E5E5",
     height: "100%",
-    width: "100%",
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  detailTitle: {
+    color: "#282C43",
+    fontWeight: "bold",
+    fontFamily: "Montserrat",
+  },
+  formCard: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    height: 160,
+    borderRadius: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 15,
+    marginBottom: 60,
   },
 });
 
@@ -19,20 +47,56 @@ const AppRouter = () => {
       <Route exact path="/">
         <Redirect to="/uploads" />
       </Route>
-      <Route exact path="/uploads">
+      <Route path="/uploads">
         <App />
       </Route>
     </Switch>
   );
-}
+};
 
-const App = () => {
+const DetailsCard = ({ files }) => {
   const styles = useStyles();
+  const { uploadId } = useParams();
+  const detailFile = files.find((file) => file.id === parseInt(uploadId));
+
   return (
     <div className={styles.appContainer}>
       <AppHeader />
-      <AppBody />
+      <div className={styles.detailBody}>
+        <h1 className={styles.detailTitle}>{detailFile?.fileName}</h1>
+        <div className={styles.formCard}>{uploadId}</div>
+      </div>
     </div>
+  );
+};
+
+const App = () => {
+  const styles = useStyles();
+  const { path } = useRouteMatch();
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    const getYards = async () => {
+      const response = await Axios.get("http://localhost:8080/uploads");
+      setFiles(response.data);
+    };
+    getYards();
+  }, [setFiles]);
+
+  console.log("Path", path);
+
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <div className={styles.appContainer}>
+          <AppHeader />
+          <AppBody files={files} setFiles={setFiles} />
+        </div>
+      </Route>
+      <Route path={`${path}/:uploadId`}>
+        <DetailsCard files={files} />
+      </Route>
+    </Switch>
   );
 };
 
