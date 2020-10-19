14d34ec74bf1edef1e660599e280d3dcb2143a77
Getulio <getuliovdmnaweb@gmail.com>
2020-10-18 21:59:37 -0300

    Adding Material Ui Core to project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <CssBaseline />
       <AppRouter />
     </Router>
   </React.StrictMode>,
